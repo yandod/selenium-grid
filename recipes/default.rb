d7a8a346368ef78bba7c1fdc77517896a0f0ed51
Yusuke Ando <yandod@gmail.com>
2014-01-02 22:32:56 +0900

    update

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,3 +13,9 @@ include_recipe 'selenium-grid::install'
 
 # ensure port 4444 is accessible
 # ensure that java is installed
+
+service "selenium-grid" do
+  start_command "java -jar #{Chef::Config[:file_cache_path]}/selenium-server-standalone-2.33.0.jar"
+  stop_command "pkill -f 'java -jar'"
+  action :start
+end
