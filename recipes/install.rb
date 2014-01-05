4b0f2c109d24dd182d04b4225dfb56381802b3ec
Yusuke Ando <yandod@gmail.com>
2014-01-05 13:43:16 +0900

    update

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,4 +1,8 @@
-remote_file "#{Chef::Config[:file_cache_path]}/selenium-server-standalone-2.33.0.jar" do
-  source "https://selenium.googlecode.com/files/selenium-server-standalone-2.33.0.jar"
+remote_file "#{Chef::Config[:file_cache_path]}/selenium-server-standalone-2.39.0.jar" do
+  source "https://selenium.googlecode.com/files/selenium-server-standalone-2.39.0.jar"
   mode 0644
 end
+
+package 'xvfb'
+package 'dbus-x11'
+package 'firefox'
