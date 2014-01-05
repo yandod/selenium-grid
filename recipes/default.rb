5fb135f69f21910d4476f55e1068f39fb65cea9a
Yusuke Ando <yandod@gmail.com>
2014-01-05 17:03:13 +0900

    update

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,8 +14,13 @@ include_recipe 'selenium-grid::install'
 # ensure port 4444 is accessible
 # ensure that java is installed
 
+service "xvfb" do
+	start_command "/usr/bin/Xvfb :1 -screen 0 1024x768x8 > /tmp/xvfb.log 2> /tmp/xvfb.error"
+	stop_command "pkill -f 'Xvfb'"
+end
+
 service "selenium-grid" do
-  start_command "/usr/bin/Xvfb :1 -screen 0 1024x768x8 > /tmp/xvfb.log 2> /tmp/xvfb.error & && export DISPLAY=:1.0 && java -jar #{Chef::Config[:file_cache_path]}/selenium-server-standalone-2.39.0.jar > /tmp/selenium.log 2> /tmp/selenium.error &"
-  stop_command "pkill -f 'java -jar' && pkill -f 'Xvfb'"
+  start_command "export DISPLAY=:1.0 && java -jar #{Chef::Config[:file_cache_path]}/selenium-server-standalone-2.39.0.jar > /tmp/selenium.log 2> /tmp/selenium.error &"
+  stop_command "pkill -f 'java -jar'"
   action :start
 end
